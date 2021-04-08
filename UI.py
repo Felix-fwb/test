8f09265b74a1320282343cf2e905a39d81753136
Felix <felix_fwb@163.com>
2021-04-08 10:47:05 +0800

    errthdf

--- UI.py
+++ UI.py
@@ -18,28 +18,28 @@ class Example(QWidget):
         self.setGeometry(800, 800, 900, 800)
         self.setWindowTitle('一起来学习')
 
-        bt1 = QPushButton('剪刀', self)
+        bt1 = QPushButton('a', self)
         # bt1.move(50, 250)
 
-        bt2 = QPushButton('石头', self)
+        bt2 = QPushButton('b', self)
         # bt2.move(150, 250)
 
-        bt3 = QPushButton('布', self)
+        bt3 = QPushButton('c', self)
         # bt3.move(250, 250)
         h_box = QHBoxLayout()
-        h_box.addStretch(2)
+        h_box.addStretch(20)
 
         h_box.addWidget(bt1)
         h_box.addStretch(1)
         h_box.addWidget(bt2)
         h_box.addStretch(1)
         h_box.addWidget(bt3)
-        h_box.addStretch(5)
+        h_box.addStretch(1)
 
         v_box = QVBoxLayout()
         v_box.addStretch(1)
         v_box.addLayout(h_box)
-        v_box.addStretch(15)
+        v_box.addStretch(1)
 
         self.setLayout(v_box)
 
