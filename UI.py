386258e2a01a0bc328cbf0a3a66e99a050a27f11
Felix <felix_fwb@163.com>
2021-04-08 16:50:09 +0800

    新的测试first

--- UI.py
+++ UI.py
@@ -35,7 +35,7 @@ class Example(QWidget):
         h_box.addStretch(1)
         h_box.addWidget(bt3)
         h_box.addStretch(1)
-        #第一次更新上去！！！！！！！
+
         v_box = QVBoxLayout()
         v_box.addStretch(1)
         v_box.addLayout(h_box)
